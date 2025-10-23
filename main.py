9c5561d31a4fd7c84a167a1440282bb31e9e33bb
daniel <dmoeller38@outlook.com>
2025-10-23 13:17:03 -0400

    Init

---
+++ main.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+# Import the necessary libraries
+import time
+import math
+from ev3dev2.motor import *
+from ev3dev2.sound import Sound
+from ev3dev2.button import Button
+from ev3dev2.sensor import *
+from ev3dev2.sensor.lego import *
+from ev3dev2.sensor.virtual import *
+
+# Create the sensors and motors objects
+motorA = LargeMotor(OUTPUT_A)
+motorB = LargeMotor(OUTPUT_B)
+left_motor = motorA
+right_motor = motorB
+tank_drive = MoveTank(OUTPUT_A, OUTPUT_B)
+steering_drive = MoveSteering(OUTPUT_A, OUTPUT_B)
+
+spkr = Sound()
+btn = Button()
+radio = Radio()
+obtr = ObjectTracker()
+
+gyro_sensor_in1 = GyroSensor(INPUT_1)
+
+
+# Here is where your code starts
+
